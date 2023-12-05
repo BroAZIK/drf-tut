3a74f6a603f450d21f82804ad5efcc8e94886bc7
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-05 17:01:06 +0500

    cbv

--- api/views.py
+++ api/views.py
@@ -1,12 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 
 
-@api_view(['GET', 'POST'])
-def home(request: Request) -> Response:
-    if request.method == 'GET':
+class HomeView(APIView):
+    def get(self, request: Request) -> Response:
         params = request.query_params
         a = params['a']
         b = params['b']
@@ -16,7 +15,7 @@ def home(request: Request) -> Response:
             status=status.HTTP_200_OK
         )
 
-    elif request.method == 'POST':
+    def post(self, request: Request) -> Response:
         data = request.data
         a = data['a']
         b = data['b']
