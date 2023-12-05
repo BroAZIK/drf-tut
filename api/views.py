92f2191697f54228c89855105f0e28953bfece13
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-05 16:56:08 +0500

    request and response

--- api/views.py
+++ api/views.py
@@ -1,17 +1,27 @@
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
 def home(request: Request) -> Response:
     if request.method == 'GET':
         params = request.query_params
-        print(params)
-        
-        return Response({'message': 'get'})
+        a = params['a']
+        b = params['b']
+
+        return Response(
+            data={'message': int(a) + int(b)}, 
+            status=status.HTTP_200_OK
+        )
 
     elif request.method == 'POST':
-        print(request.data)
+        data = request.data
+        a = data['a']
+        b = data['b']
 
-        return Response({'data': 'post'})
+        return Response(
+            data={'data': int(a) + int(b)}, 
+            status=status.HTTP_201_CREATED
+        )
