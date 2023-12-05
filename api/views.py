34ba8500077515500055ed788e8a06ca276009e5
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-05 09:57:59 +0500

    request and response

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def home(request: Request) -> Response:
+    if request.method == 'GET':
+        print(request.query_params)
+
+        return Response({"name": request.query_params['name']})
+
+    elif request.method =='POST':
+        return Response({"name": request.data['name']})
+
